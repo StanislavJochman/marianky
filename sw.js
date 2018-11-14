408fd025bf43f3226785b94a99a38a1d93472e9e
StanislavJochman <s.jochman@gmail.com>
2018-11-14 17:57:53 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -35,34 +35,3 @@ this.addEventListener('fetch', event => {
       }
 });
 
-/*
-
-
-const version = "0.6.11";
-const cacheName = `marianky-${version}`;
-self.addEventListener('install', e => {
-  const timeStamp = Date.now();
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll([
-        `index.html`
-      ])
-          .then(() => self.skipWaiting());
-    })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.open(cacheName)
-      .then(cache => cache.match(event.request, {ignoreSearch: true}))
-      .then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-*/
